f5c9ba3f74993964f551489a92bb9eaee3fdc663
Kazu Yamamoto <kazu@iij.ad.jp>
2020-05-14 11:44:24 +0900

    specifying tlsClientCertificate.

--- Network/Wai/Handler/WarpQUIC.hs
+++ Network/Wai/Handler/WarpQUIC.hs
@@ -14,12 +14,13 @@ runQUIC quicsettings settings app = do
     withII settings $ \ii ->
         runQUICServer quicsettings $ \conn -> do
            info <- getConnectionInfo conn
+           mccc <- clientCertificateChain conn
            let addr = remoteSockAddr info
            let transport = TLS {
                    tlsMajorVersion = 3
                  , tlsMinorVersion = 4
                  , tlsNegotiatedProtocol = alpn info
                  , tlsChiperID = cipherID $ cipher info
-                 , tlsClientCertificate = Nothing
+                 , tlsClientCertificate = mccc
                  }
            H3.run conn $ http2server settings ii transport addr app
